597f68b19b983ba0f036c28b1078756ef891357b
BettyRoot <vburzynska@outlook.com>
2018-07-09 11:51:33 +0200

    welcome- cookie username

--- routes/cards.js
+++ routes/cards.js
@@ -6,18 +6,26 @@ const { cards } = data;
 router.get('/', (req, res) => {
     var cardsLength = cards.length;
     var randomCard = Math.floor(Math.random()*cardsLength);
-    res.redirect(`/cards/${randomCard}?side=question`);
-
-})
+    res.redirect(`/cards/${randomCard}`);
+});
 
 
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+    if(!side)
+    {
+
+        res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+
+
+
     const text = cards[id][side];
     const { hint } = cards[id];
     
-    const templateData = { id, text };
+    const templateData = { id, text, name};
 
     if (side === 'question') {
         templateData.sideToShow = 'answer';
